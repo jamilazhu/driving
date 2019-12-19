6f3dbbf6ef9a0ec0f4bf08969974a3fed91b391d
Yizhen Zhu <jamilazhu@gmail.com>
2019-12-19 14:37:45 +1100

    change China

--- driving.py
+++ driving.py
@@ -1,10 +1,15 @@
 country = input('Which country are you from : ')
-age = input(' Please enter your age : ')
+age = input('Please enter your age : ')
 age = int(age)
 if country == 'China':
     if age >= 18:
-        print('you can conduct the test')
+        print('You can conduct the test')
     else:
-        print('you can not yet conduct the test')
+        print('You can not yet conduct the test')
+elif country == 'USA':
+    if age >= 16:
+        print('You can conduct the test')
+    else:
+        print('You can not yet conduct the test')
 
         
